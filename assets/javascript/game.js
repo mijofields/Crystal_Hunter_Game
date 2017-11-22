cc36774e5d0cd45f0e0c195b4b38d1a7034b5882
El Pirata Mike <mikefields81@gmail.com>
2017-11-21 21:54:57 -0600

    base files

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+			//make this game out of objects
+
+	var game = {
+
+	crystals: [blue, yellow, red, black]
+
+	targetScore: function targetScore (min, max) {
+
+	min = Math.ceil(19);
+	max = Math.floor(120);
+			  
+	Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+	}
+
+
+
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
