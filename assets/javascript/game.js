a0e827c6b80153b2e8b0badcb72674e3cb282b6e
El Pirata Mike <mikefields81@gmail.com>
2017-11-22 21:13:10 -0600

    starting to build objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,56 @@
 			//make this game out of objects
 
+	
+
 	var game = {
 
-	crystals: [blue, red, yello, purple]
+	crystals: ["blue", "red", "yellow", "purple"],
+	crystalValues: [],
 
-	targetScore: function targetScore (min, max) {
+	targetScore: function targetScore (min, max) {  // random number in min, max range inclusive
 
 	min = Math.ceil(19);
 	max = Math.floor(120);
 			  
-	Math.floor(Math.random() * (max - min + 1)) + min;
+	console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 
 
-	}
+	}, //end of targetScore ()
+
+	crystalValue: function crystalValue (min, max) {  // random number in min, max range inclusive
 
 
+		min = Math.ceil(1);
+		max = Math.floor(12);
+
+
+		for (i=0; this.crystalValues.length < this.crystals.length; i++){
+
+		randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
+		this.crystalValues.push(randomValue);
+		this.crystalValues = $.unique(this.crystalValues);
+
+		}
+		}, //end of crystalValue
+
+
+	playerScore: 0,
+
+	wins: 0,
+
+	losses: 0
 
 			}
 
 
+			
+			console.log(game.crystals);
+			console.log(game.targetScore()); // why do I need to add these to cause the console.log in the function to work?
+			console.log(game.crystalValue()); // why do I need to add these to cause the console.log in the function to work?
+			console.log(game.playerScore);
+			console.log(game.wins);
+			console.log(game.losses);
+			console.log(game.crystalValues);
 
 
-
-
-
-
-
-
-
-
-
+			
